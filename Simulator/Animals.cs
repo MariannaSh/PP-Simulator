550d344603680a92233e1323bf2bd0ff6edaa615
Marianna <226659@student.uek.krakow.pl>
2024-10-29 21:01:26 +0300

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,8 +2,43 @@
 
 internal class Animals
 {
-    public required string Description { get; init; }
+    private string _description = "Unknown";
     public uint Size { get; set; } = 3;
+
+    public required string Description
+    {
+        get => _description;
+
+        init
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                value = "###";
+            }
+            else
+            {
+                value = value.Trim();
+
+                if (value.Length > 15) 
+                {
+                    value = value.Substring(0, 15).TrimEnd();
+                }
+
+                while (value.Length < 3)
+                {
+                    value += "#";
+                }
+            }
+
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+
+            _description = value;
+        }
+    }
 
     public string Info => $"{Description} <{Size}>";
 }
+
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,18 +2,84 @@
 
 internal class Creature
 {
-    public string  Name {  get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level = 1;
 
-    public Creature(string name, int level = 1)
+    public string Name
+    {
+        get => _name;
+        init
+        {
+
+            if (value == null) 
+            {
+                value = "###"; 
+            }
+            else
+            {
+                value = value.Trim(); 
+            }
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                _name = "###";
+            }
+            else
+            {
+                
+                if (value.Length > 25)
+                {
+                    value = value.Substring(0, 25).TrimEnd();
+                }
+
+                
+                if (value.Length < 3)
+                {
+                    value = value.PadRight(3, '#');
+                }
+
+                
+                if (char.IsLower(value[0]))
+                {
+                    value = char.ToUpper(value[0]) + value.Substring(1);
+                }
+
+                _name = value; 
+            }
+        }
+    }
+
+
+    public int Level
+    {
+        get => _level;
+        init
+        {
+            if (value < 1)
+                _level = 1;
+            else if (value > 10)
+                _level = 10;
+            else
+                _level = value;
+        }
+    }
+
+    public void Upgrade()
+    {
+        if (_level < 10)
+        {
+            _level++;
+        }
+    }
+
+
+    public Creature(string name = "Unknown", int level = 1)
     {
         Name = name;
         Level = level;
     }
 
-    public Creature()
-    {
-    }
+    public Creature() { }
 
     public void SayHi()
     {
